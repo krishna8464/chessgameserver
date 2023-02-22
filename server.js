d5bf3142184726bcf0d0544e29ab9ea76e1848e0
YENDA VAMSHI KRISHNA <yendavamshikrishna123@gmail.com>
2023-02-23 00:48:49 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const app = express();
+const server = require("http").Server(app);
+const io = require("socket.io")(server);
+const {v4 : uuidV4} = require("uuid")
+
+app.set('view engine', 'ejs');
+app.use(express.static("public"));
+
+
+app.get("/",(req,res)=>{
+  res.redirect(`/${uuidV4()}`)
+})
+
+app.get("/:room",(req,res)=>{
+    res.render("room",{roomId : req.params.room})
+})
+
+
+io.on("connection",socket =>{
+  socket.on("join-room",(roomId,userId,user)=>{
+    socket.join(roomId)
+    // socket.broadcast.emit("wickets",`${wickets}`)
+    setTimeout(() => {
+      socket.broadcast.to(roomId).emit("user-connected", userId);
+    }, 1000);
+    
+    socket.on("message", (message) => {
+      io.to(roomId).emit("createMessage", message,user);
+    });
+
+    socket.on('disconnect',()=>{
+      socket.broadcast.to(roomId).emit("user-disconnected", userId);
+    })
+  })
+})
+
+server.listen(5050)
